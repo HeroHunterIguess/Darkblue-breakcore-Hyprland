7bb7faa3acdb755b04db55c3144f46992033a5d2
Raylee <abramscarter1999@gmail.com>
2025-11-07 15:20:52 -0500

    create fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# calculator alias'
+alias calc='cd "/home/herohunter/Everything/Coding/rust projects/" && rustc FINALcalculator.rs && "/home/herohunter/Everything/Coding/rust projects/"FINALcalculator'
+
+alias pythoncalc='python -u "/home/herohunter/Everything/Coding/better coding lol/new pc coding/calculator.py"'
+
+
+
+# cd into folder for waves game files
+alias cdw='cd ~/Everything/godot\ projects/waves'
