bdf4c11d1db12662470d56060ab8b50c54a65d3b
Raylee <abramscarter1999@gmail.com>
2026-02-10 20:27:50 -0500

    add newline echo after command execution

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# echo that after command finishes
+function add_newline_after_command --on-event fish_postexec
+    echo "-------------------"
+end
+
 # calculator alias'
 alias calc='cd "/home/herohunter/Everything/Coding/rust projects/" && rustc FINALcalculator.rs && "/home/herohunter/Everything/Coding/rust projects/"FINALcalculator'
 
